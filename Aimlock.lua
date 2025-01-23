30d00906a4f9c276fea3dcc578f810cd9c668ff1
CHKNPinkhat <tllpinkhat@gmail.com>
2025-01-23 01:29:05 +0100

    Update Aimlock.lua

--- Aimlock.lua
+++ Aimlock.lua
@@ -17,11 +17,7 @@ _G.CircleRadius = 80 -- The radius of the circle / FOV.
 _G.CircleFilled = false -- Determines whether or not the circle is filled.
 _G.CircleThickness = 0 -- The thickness of the circle.
 
-if _G.AimbotEnabled = false then
-FOVCircle.Destroy()
-end
-
-if _G.AimbotEnabled = true then
+if _G.AimbotEnabled then
 local FOVCircle = Drawing.new("Circle")
 FOVCircle.Position = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
 FOVCircle.Radius = _G.CircleRadius
@@ -32,6 +28,9 @@ FOVCircle.Radius = _G.CircleRadius
 FOVCircle.Transparency = _G.CircleTransparency
 FOVCircle.NumSides = _G.CircleSides
 FOVCircle.Thickness = _G.CircleThickness
+else
+FOVCIRCLE.Destroy()
+end
 
 local function GetClosestPlayer()
 	local MaximumDistance = _G.CircleRadius
